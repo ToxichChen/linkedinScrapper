c9f47ccdae86350863a7d6d7690c0bfc0a501413
ToxichChen <toxich.chepurenko@gmail.com>
2022-06-22 19:01:23 +0300

    airtableParser.js added

---
+++ database/migrations/2022_06_22_154740_create_crypto_companies_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('crypto_companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('fund_raising_round');
+            $table->string('date');
+            $table->string('amount');
+            $table->string('investors');
+            $table->string('website');
+            $table->string('category');
+            $table->string('subcategories');
+            $table->string('description');
+            $table->string('stages');
+            $table->string('valuation');
+            $table->string('project');
+            $table->string('announcement');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('crypto_companies');
+    }
+};
---
+++ node_scripts/scripts/airtableParser.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql');
+const Airtable = require('airtable');
+const credentials = require('../credentials.js')
+
+let connection = mysql.createConnection({
+    host: credentials.dbHost,
+    user: credentials.dbUser,
+    password: credentials.dbPassword,
+    database: credentials.database
+});
+
+let base = new Airtable({apiKey: 'keytvFhqneWKMyPnX'}).base('appzJ0hfvu6z2s5Fc');
+
+base('tbl0XENq0nILbadRU').select({
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+
+    records.forEach(function(record) {
+        let sql = (`INSERT INTO crypto_companies (fund_raising_round, date, amount, investors, website, category, subcategories, description, stages, valuation, project, announcement)
+                    VALUES ("${record.get('Fundraising Round')}", "${record.get('Date')}", "${record.get('Amount') || ''}", "${record.get('Investors')  || ''}", "${record.get('Website')}", "${record.get('Category')}", "${record.get('Sub-categories')}", "${record.get('Description')}", "${record.get('Stages')}", "${(record.get('Valuation') || '')}", "${record.get('Project')}", "${record.get('Announcement') || ''}") `);
+        connection.query(sql, function (err, result) {
+            if (err) {
+                throw err;
+            } else {
+                console.log("Successfully saved!")
+            }
+        });
+        console.log('Retrieved', record.get('Investors'));
+    });
+
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); connection.end(); }
+    connection.end();
+});
